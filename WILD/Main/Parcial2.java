143a0787cf3ee5fa4689234df0133b801d0307c3
Kamil Jofré <kamiljelias@gmail.com>
2026-06-03 19:37:05 -0300

    -
    Arbol terminado

--- WILD/Main/Parcial2.java
+++ WILD/Main/Parcial2.java
@@ -27,7 +27,7 @@ public class Parcial2 {
                     
                         nocturno.setSi(new Nodo("Buho"));
                         
-                        Nodo velocidad = new Nodo("Tu animal es conocido por su velocidad?");
+                        Nodo velocidad = new Nodo("Tu animal es considerado el vae mas rapida del mundo?");
                         nocturno.setNo(velocidad);
                         
                             velocidad.setSi(new Nodo("Halcon"));
@@ -56,65 +56,135 @@ public class Parcial2 {
                 Nodo mamifero = new Nodo("Tu animal es mamifero?");
                 plumas.setNo(mamifero);
                 
-                    Nodo domestico = new Nodo("Tu animal es domestico?");
-                    mamifero.setSi(domestico);
+                    Nodo trompa =  new Nodo("Tu aniamal tiene trompa?");
+                    mamifero.setSi(trompa);
                     
-                        Nodo amigo = new Nodo("Tu animal es considerado el mejor amigo del hombre?");
-                        domestico.setSi(amigo); 
+                        trompa.setSi(new Nodo("Elefante"));
                         
-                            amigo.setSi(new Nodo("Perro"));
-                            amigo.setNo(new Nodo("Gato"));
+                        Nodo cuelloMamifero = new Nodo("Tu animal tiene el cuello muy largo?");
+                        trompa.setNo(cuelloMamifero);
+                        
+                            cuelloMamifero.setSi(new Nodo("Jirafa"));
                             
-                        Nodo deGranja = new Nodo("Tu animal es considerado de granja?");
-                        domestico.setNo(deGranja);
+                            Nodo domestico = new Nodo("Tu animal es domestico?");
+                            cuelloMamifero.setNo(domestico);
+
+                                Nodo ladra = new Nodo("Tu animal ladra?");
+                                domestico.setSi(ladra);
+
+                                    ladra.setSi(new Nodo("Perro"));
+
+                                    Nodo maulla= new Nodo("Tu animal maulla?");
+                                    ladra.setNo(maulla);
+
+                                        maulla.setSi(new Nodo("Gato"));
+
+                                        Nodo lana = new Nodo("Tu animal produce lana?");
+                                        maulla.setNo(lana);
+                                        //rayas
+                                            lana.setSi(new Nodo("Oveja"));
+
+                                            Nodo relincha = new Nodo("Tu animal relincha?");
+                                            lana.setNo(relincha);
+
+                                                relincha.setSi(new Nodo("Caballo"));
+
+                                                Nodo hocico = new Nodo("Tu animal tiene el hocico chato?");
+                                                relincha.setNo(hocico);
+
+                                                    hocico.setSi(new Nodo("Cerdo"));
+                                                    hocico.setNo(new Nodo("Vaca"));
+                                                        
+                                Nodo melena = new Nodo("Tu aniaml tiene melena?");
+                                domestico.setNo(melena);
+
+                                    melena.setSi(new Nodo("Leon"));
+
+                                    Nodo primate = new Nodo("Tu animal es un primate?");
+                                    melena.setNo(primate);
+
+                                        Nodo robustez = new Nodo("Tu animal es muy robusto?");
+                                        primate.setSi(robustez);
+
+                                            robustez.setSi(new Nodo("Gorila"));
+                                            robustez.setNo(new Nodo("Mono"));
+
+                                        Nodo byn = new Nodo("Tu animal tiene rayas blancas y negras?");
+                                        primate.setNo(byn);
+
+                                            byn.setSi(new Nodo("Cebra"));
+                                            
+                                            Nodo rayas = new Nodo("Tu animal tiene rayas?");
+                                            byn.setNo(rayas);
+
+                                                rayas.setSi(new Nodo("Tigre"));
+                                            
+                                                Nodo mar = new Nodo("Tu animal vive en el mar?");
+                                                rayas.setNo(mar);
+
+                                                    Nodo enorme = new Nodo("Tu animal es enorme?");
+                                                    mar.setSi(enorme);
+
+                                                        enorme.setSi(new Nodo("Ballena"));
+                                                        enorme.setNo(new Nodo("Delfin"));
+
+                                                    mar.setNo(new Nodo("Oso"));
+                             
+                    Nodo aguaMamifero = new Nodo("Tu animal vive principalmente en el agua?");
+                    mamifero.setNo(aguaMamifero);        
+                                
+                        Nodo aplanado = new Nodo("Tu animal tiene cuerpo aplanado?");
+                        aguaMamifero.setSi(aplanado);
                         
-                            Nodo comestible  = new Nodo("Tu animal se come haitualmente?");
-                            deGranja.setSi(comestible);
+                            aplanado.setSi(new Nodo("Mantarraya"));
+                            
+                            Nodo depredador =  new Nodo("Tu animal es un gran depredador?");
+                            aplanado.setNo(depredador);
+                            
+                                depredador.setSi(new Nodo("Tiburon"));
+                                depredador.setNo(new Nodo("Atun"));
                                 
-                                Nodo leche = new Nodo("Tu animal consume leche que luego es consimda por seres humanos?");
-                                comestible.setSi(leche);
+                        Nodo escamosa = new Nodo("Tu animal tiene la piel escamoza?");
+                        aguaMamifero.setNo(escamosa);
+                            
+                            Nodo caparazon = new Nodo("Tu animal tiene caparazon?");
+                            escamosa.setSi(caparazon);
+                            
+                                caparazon.setSi(new Nodo("Tortuga"));
                                 
-                                    Nodo lana = new Nodo("Tu animal produce lana?");
-                                    leche.setSi(lana);
-                                        
-                                        lana.setSi(new Nodo("Oveja"));
-                                        lana.setNo(new Nodo("Vaca"));
+                                Nodo patas = new Nodo("Tu animal tiene patas?");
+                                caparazon.setNo(patas);
+                                
+                                    Nodo color =  new Nodo("Tu animal cambia de color?");
+                                    patas.setSi(color);
                                     
-                                    leche.setNo(new Nodo("Cerdo"));
+                                        color.setSi(new Nodo("Camaleon"));
+                                        
+                                        Nodo viveAgua = new Nodo("Tu animal vive cerca del agua?");
+                                        color.setNo(viveAgua);
+                                        
+                                            viveAgua.setSi(new Nodo("Cocodrilo"));
+                                            viveAgua.setNo(new Nodo("Iguana"));
+                                            
+                                    patas.setNo(new Nodo("Serpiente"));
+                                    
+                            Nodo adultez = new Nodo("Tu animal tiene cola en la adultez?");
+                            escamosa.setNo(adultez);
                             
-                            Nodo montura = new Nodo("Tu animal se utiliza habitualmente para montar?");
-                            deGranja.setNo(montura);
+                                adultez.setSi(new Nodo("Salamandra"));
+                                adultez.setNo(new Nodo("Sapo"));
                                 
-                                montura.setSi(new Nodo("Caballo"));
-                            
-                                
-                    Nodo agua1 = new Nodo("Tu animal vive principalmente en el agua?");
-                    mamifero.setNo(agua1);        
-                                
-                    //No es mamifero
-                    raiz.getSi().getSi().setNo(new Nodo("Tu animal vive principalmente en el agua?"));
-                        //Si es mamifero
-                            //Tiburón / Atún / Raya
-                        
-                        //No vive en el agua
-                        raiz.getSi().getSi().getNo().setNo(new Nodo("Tu animal tiene la piel escamosa?"));
-                            //Si tiene piel escamosa
-                                //Cocodrilo / Lagarto / Iguana / Tortuga / Serpiente
-                                
-                            //No tiene piel escamosa
-                                //Rana / Sapo / Salamandra
-                                
-            Nodo patas = new Nodo("Tu animal tiene 8 patas?");
-            raiz.setNo(patas);
+            Nodo patas8 = new Nodo("Tu animal tiene 8 patas?");
+            raiz.setNo(patas8);
             
                 Nodo cola = new Nodo("Tu animal tiene cola");
-                patas.setSi(cola);
+                patas8.setSi(cola);
                 
                     cola.setSi(new Nodo("Escorpion"));
                     cola.setNo(new Nodo("Araña"));
                     
                 Nodo volar = new Nodo("Tu animal puede volar?");
-                patas.setNo(volar);
+                patas8.setNo(volar);
                 
                     Nodo pica = new Nodo("Tu animal pica?");
                     volar.setSi(pica);
@@ -124,7 +194,7 @@ public class Parcial2 {
                         
                             amarillo.setSi(new Nodo("Abeja"));
                             amarillo.setNo(new Nodo("Mosquito"));
-                            
+                            //halcon
                         pica.setNo(new Nodo("Mariposa"));
                         
                     Nodo agua = new Nodo("Tu animal vive en el agua?");
@@ -151,32 +221,6 @@ public class Parcial2 {
                             
                                 sangre.setSi(new Nodo("Garrapata"));
                                 sangre.setNo(new Nodo("Hormiga"));
-                
+                 
     }
 }
-/*
-¿Tiene columna vertebral?
-├─ Sí
-│  ├─ ¿Tiene plumas?
-│  │  ├─ Sí
-│  │  │  ├─ ¿Es ave de presa?
-│  │  │  │  ├─ Sí → Águila / Halcón / Búho
-│  │  │  │  └─ No → Loro / Paloma / Gallina / Pato / Cisne
-│  │
-│  └─ No
-│     ├─ ¿Es mamífero?
-│     │  ├─ Sí
-│     │  │  ├─ ¿Es doméstico?
-│     │  │  │  ├─ Sí → Perro / Gato / Caballo / Vaca / Oveja / Cerdo
-│     │  │  │  └─ No → León / Tigre / Elefante / Jirafa / Cebra
-│     │
-│     │  └─ No
-│     │     ├─ ¿Vive principalmente en el agua?
-│     │     │  ├─ Sí → Tiburón / Atún / Raya
-│     │     │  └─ No
-│     │     │     ├─ ¿Tiene piel escamosa?
-│     │     │     │  ├─ Sí → Cocodrilo / Lagarto / Iguana / Tortuga / Serpiente
-│     │     │     │  └─ No → Rana / Sapo / Salamandra
-│
-
-*/
